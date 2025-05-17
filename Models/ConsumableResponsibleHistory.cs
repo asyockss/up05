4c61651ec3847d6bbf08e38bdb1c86c8cb1a1bde
avyakovleva <nastya28042020@gmail.com>
2025-05-17 22:14:19 +0500

    Модель истории ответсвенныъ за расходники

---
+++ Models/ConsumableResponsibleHistory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory.Models
+{
+    public class ConsumableResponsibleHistory
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("Consumable")]
+        public int ConsumableId { get; set; }
+        public virtual Consumable Consumable { get; set; }
+
+        [ForeignKey("OldUser")]
+        public int? OldUserId { get; set; }
+        public virtual User OldUser { get; set; }
+
+        public DateTime ChangeDate { get; set; }
+    }
+}
