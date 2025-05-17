2fbcc4655e8647be25be8e0a6036afb72af9405e
avyakovleva <nastya28042020@gmail.com>
2025-05-17 22:04:59 +0500

    Модель истории оборудования

---
+++ Models/EquipmentLocationHistory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory.Models
+{
+    public class EquipmentLocationHistory
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("Equipment")]
+        public int EquipmentId { get; set; }
+        public virtual Equipment Equipment { get; set; }
+
+        [ForeignKey("Room")]
+        public int? RoomId { get; set; }
+        public virtual Room Room { get; set; }
+
+        public DateTime ChangeDate { get; set; }
+        public string Comment { get; set; }
+    }
+}
