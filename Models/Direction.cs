575ed6f144ac23b0cd6b1ceb7ec68d9a6263c830
vozhakovanatasha <vozhakovatasha1@gmail.com>
2025-05-18 15:59:35 +0300

    создала Direction

---
+++ Models/Direction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory.Models
+{
+    public class Direction
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Название направления обязательно")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Equipment> Equipment { get; set; }
+
+        public Direction()
+        {
+            Equipment = new HashSet<Equipment>();
+        }
+    }
+}
+}
