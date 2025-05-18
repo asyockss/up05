5e52f7686d0c58b7011898433c1c229e6d25c279
vozhakovanatasha <vozhakovatasha1@gmail.com>
2025-05-18 15:29:40 +0300

    создала Room

---
+++ Models/Room.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Название аудитории обязательно")]
+        public string Name { get; set; }
+
+        public string ShortName { get; set; }
+
+        [ForeignKey("ResponsibleUser")]
+        public int? ResponsibleId { get; set; }
+        public virtual User ResponsibleUser { get; set; }
+
+        [ForeignKey("TempResponsibleUser")]
+        public int? TempResponsibleId { get; set; }
+        public virtual User TempResponsibleUser { get; set; }
+
+        public virtual ICollection<Equipment> Equipment { get; set; }
+
+        public Room()
+        {
+            Equipment = new HashSet<Equipment>();
+        }
+    }
+}
+}
