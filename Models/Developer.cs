bff7c6804455a4d951d2273dd8a42e42e6e76693
vozhakovanatasha <vozhakovatasha1@gmail.com>
2025-05-18 16:17:43 +0300

    создала Developer

---
+++ Models/Developer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory.Models
+{
+    public class Developer
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Название разработчика обязательно")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Software> Software { get; set; }
+
+        public Developer()
+        {
+            Software = new HashSet<Software>();
+        }
+    }
+}
+}
