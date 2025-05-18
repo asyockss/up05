089742552a091b22d5f8a21f54dae6a75abce20f
vozhakovanatasha <vozhakovatasha1@gmail.com>
2025-05-18 16:29:33 +0300

    создала InventoryCheck

---
+++ Models/InventoryCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory.Models
+{
+    public class InventoryCheck
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("Inventory")]
+        public int InventoryId { get; set; }
+        public virtual Inventory Inventory { get; set; }
+
+        [ForeignKey("Equipment")]
+        public int EquipmentId { get; set; }
+        public virtual Equipment Equipment { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public DateTime CheckDate { get; set; }
+        public string Comment { get; set; }
+    }
+}
