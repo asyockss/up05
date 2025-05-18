564715cd26df76f1e90818d4471aa2379b08c726
vozhakovanatasha <vozhakovatasha1@gmail.com>
2025-05-18 16:14:09 +0300

    создала Software

---
+++ Models/Software.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory.Models
+{
+    public class Software
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Название программы обязательно")]
+        public string Name { get; set; }
+
+        public string Version { get; set; }
+
+        [ForeignKey("Developer")]
+        public int? DeveloperId { get; set; }
+        public virtual Developer Developer { get; set; }
+
+        [ForeignKey("Equipment")]
+        public int? EquipmentId { get; set; }
+        public virtual Equipment Equipment { get; set; }
+    }
+}
+}
