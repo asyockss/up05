2ad636a7aa989e8f3267a595afcb39278022b80a
vozhakovanatasha <vozhakovatasha1@gmail.com>
2025-05-18 16:11:35 +0300

    создала EquipmentModel

---
+++ Models/EquipmentModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory.Models
+{
+    public class EquipmentModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Название модели обязательно")]
+        public string Name { get; set; }
+
+        [ForeignKey("EquipmentType")]
+        public int EquipmentTypeId { get; set; }
+        public virtual EquipmentType EquipmentType { get; set; }
+
+        public virtual ICollection<Equipment> Equipment { get; set; }
+
+        public EquipmentModel()
+        {
+            Equipment = new HashSet<Equipment>();
+        }
+    }
+}
+}
