297b3782b3bf15f305726f89f4bb747dc337fc2e
vozhakovanatasha <vozhakovatasha1@gmail.com>
2025-05-19 14:55:21 +0300

    настройка связей между таблицами

--- Context/InventoryContext.cs
+++ Context/InventoryContext.cs
@@ -35,5 +35,104 @@ namespace inventory.Context
         public DbSet<Software> Software { get; set; }
         public DbSet<Status> Statuses { get; set; }
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Настройка связей между таблицами
+            modelBuilder.Entity<Consumable>()
+                .HasMany(c => c.Characteristics)
+                .WithRequired(c => c.Consumable)
+                .HasForeignKey(c => c.ConsumableId);
+
+            modelBuilder.Entity<Consumable>()
+                .HasMany(c => c.ResponsibleHistory)
+                .WithRequired(c => c.Consumable)
+                .HasForeignKey(c => c.ConsumableId);
+
+            modelBuilder.Entity<Equipment>()
+                .HasMany(e => e.EquipmentConsumables)
+                .WithRequired(e => e.Equipment)
+                .HasForeignKey(e => e.EquipmentId);
+
+            modelBuilder.Entity<Equipment>()
+                .HasMany(e => e.LocationHistory)
+                .WithRequired(e => e.Equipment)
+                .HasForeignKey(e => e.EquipmentId);
+
+            modelBuilder.Entity<Equipment>()
+                .HasMany(e => e.ResponsibleHistory)
+                .WithRequired(e => e.Equipment)
+                .HasForeignKey(e => e.EquipmentId);
+
+            modelBuilder.Entity<Equipment>()
+                .HasMany(e => e.NetworkSettings)
+                .WithRequired(e => e.Equipment)
+                .HasForeignKey(e => e.EquipmentId);
+
+            modelBuilder.Entity<Equipment>()
+                .HasMany(e => e.Software)
+                .WithOptional(e => e.Equipment)
+                .HasForeignKey(e => e.EquipmentId);
+
+            modelBuilder.Entity<Inventory>()
+                .HasMany(i => i.Checks)
+                .WithRequired(i => i.Inventory)
+                .HasForeignKey(i => i.InventoryId);
+
+            modelBuilder.Entity<Room>()
+                .HasMany(r => r.Equipment)
+                .WithOptional(e => e.Room)
+                .HasForeignKey(e => e.RoomId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.ResponsibleForEquipment)
+                .WithOptional(e => e.ResponsibleUser)
+                .HasForeignKey(e => e.ResponsibleId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.TempResponsibleForEquipment)
+                .WithOptional(e => e.TempResponsibleUser)
+                .HasForeignKey(e => e.TempResponsibleId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.ResponsibleForRooms)
+                .WithOptional(r => r.ResponsibleUser)
+                .HasForeignKey(r => r.ResponsibleId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.TempResponsibleForRooms)
+                .WithOptional(r => r.TempResponsibleUser)
+                .HasForeignKey(r => r.TempResponsibleId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.ResponsibleForConsumables)
+                .WithOptional(c => c.ResponsibleUser)
+                .HasForeignKey(c => c.ResponsibleId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.TempResponsibleForConsumables)
+                .WithOptional(c => c.TempResponsibleUser)
+                .HasForeignKey(c => c.TempResponsibleId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Inventories)
+                .WithRequired(i => i.User)
+                .HasForeignKey(i => i.UserId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.InventoryChecks)
+                .WithRequired(i => i.User)
+                .HasForeignKey(i => i.UserId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.EquipmentResponsibleHistory)
+                .WithOptional(e => e.OldUser)
+                .HasForeignKey(e => e.OldUserId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.ConsumableResponsibleHistory)
+                .WithOptional(c => c.OldUser)
+                .HasForeignKey(c => c.OldUserId);
+        }
     }
 }
