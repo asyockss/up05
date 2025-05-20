6ded3007fd9bec13a1d76f8248d99160e6eacffa
vozhakovanatasha <vozhakovatasha1@gmail.com>
2025-05-20 10:31:18 +0300

    интерфейс IInventoryCheck

---
+++ Interfase/IInventoryCheck.cs
@@ -0,0 +1,16 @@
+﻿using inventory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory.Interfase
+{
+    public interface IInventoryCheck
+    {
+        List<InventoryCheck> AllInventoryChecks();
+        void Save(bool Update = false);
+        void Delete();
+    }
+}
